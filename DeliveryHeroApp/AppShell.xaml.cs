99cd9c2b8a6be000691ed635af2bb31d3c24c08f
Kevin Gómez <kevingomezh@gmail.com>
2021-01-13 21:40:38 +0100

    Initial commit

---
+++ DeliveryHeroApp/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using DeliveryHeroApp.ViewModels;
+using DeliveryHeroApp.Views;
+using Xamarin.Forms;
+
+namespace DeliveryHeroApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
