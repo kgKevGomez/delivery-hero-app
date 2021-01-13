198eb20e49e66c63ce4ba96f1a53bd7cf3b269a2
Kevin Gómez <kevingomezh@gmail.com>
2021-01-13 23:18:21 +0100

    Route Stop WIP

--- DeliveryHeroApp.Core/Models/RouteStop.cs
+++ DeliveryHeroApp.Core/Models/RouteStop.cs
@@ -4,5 +4,14 @@ namespace DeliveryHeroApp.Core
 {
     public class RouteStop
     {
+        public string CustomerName { get; set; }
+    }
+
+    public class Address
+    {
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string PostalCode { get; set; }
+        public string Remarks { get; set; }
     }
 }
